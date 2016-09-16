2856add2bcd454cf8b6081c8792f3e35f38442c5
JL Lacar <joseluis.lacar@gmail.com>
2016-09-16 13:47:41 -0400

    ABCDGuesser1 finished

--- Pseudoscience/src/ABCDGuesser1.java
+++ Pseudoscience/src/ABCDGuesser1.java
@@ -5,9 +5,9 @@ import components.simplewriter.SimpleWriter1L;
 
 /**
  * Put a short phrase describing the program here.
- * 
+ *
  * @author Put your name here
- * 
+ *
  */
 public final class ABCDGuesser1 {
 
@@ -19,16 +19,28 @@ public final class ABCDGuesser1 {
 
     /**
      * Main method.
-     * 
+     *
      * @param args
      *            the command line arguments
      */
     public static void main(String[] args) {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
-        /*
-         * Put your main program code here
-         */
+
+        double mu = getMu(in, out);
+        out.println(
+                "Please enter your four personal numbers (must be greater than 1): ");
+        out.print("Enter W: ");
+        double w = in.nextDouble();
+        out.print("Enter X: ");
+        double x = in.nextDouble();
+        out.print("Enter Y: ");
+        double y = in.nextDouble();
+        out.print("Enter Z: ");
+        double z = in.nextDouble();
+
+        calculateJager(w, x, y, z, mu, in, out);
+
         /*
          * Close input and output streams
          */
@@ -36,4 +48,74 @@ public final class ABCDGuesser1 {
         out.close();
     }
 
+    /**
+     * Gets desired value of mu.
+     *
+     * @param in
+     * @param out
+     * @return mu
+     */
+    public static double getMu(SimpleReader in, SimpleWriter out) {
+        out.print("Enter a value for mu: ");
+        double mu = in.nextDouble();
+        return mu;
+    }
+
+    public static void calculateJager(double w, double x, double y, double z,
+            double mu, SimpleReader in, SimpleWriter out) {
+        double[] jager = { -5, -4, -3, -2, -1, -(1 / 2), -(1 / 3), -(1 / 4), 0,
+                (1 / 4), (1 / 3), (1 / 2), 1, 2, 3, 4, 5 };
+        double bestEstimate = 0, currentEstimate, bestA = 1, bestB = 1,
+                bestC = 1, bestD = 1;
+        int i = 0, j = 0, k = 0, l = 0;
+
+        while (i < jager.length) {
+            double a = jager[i];
+            j = 0;
+            while (j < jager.length) {
+                double b = jager[j];
+                k = 0;
+                while (k < jager.length) {
+                    double c = jager[k];
+                    l = 0;
+                    while (l < jager.length) {
+                        double d = jager[l];
+                        currentEstimate = Math.pow(w, a) * Math.pow(x, b)
+                                * Math.pow(y, c) * Math.pow(z, d);
+                        if (Math.abs(mu - currentEstimate) < Math
+                                .abs(mu - bestEstimate)) {
+                            bestEstimate = currentEstimate;
+
+                            bestA = a;
+                            bestB = b;
+                            bestC = c;
+                            bestD = d;
+                        }
+                        l++;
+                    }
+                    k++;
+                }
+                j++;
+            }
+            i++;
+        }
+
+        double error = percentError(mu, bestEstimate);
+        out.println("Optimized exponents: " + bestA + ", " + bestB + ", "
+                + bestC + ", " + bestD);
+        out.println("Jager formula value: " + bestEstimate);
+        out.println("Relative error: " + error);
+    }
+
+    /**
+     * Calculates percent error.
+     *
+     * @param a
+     * @param b
+     * @return error
+     */
+    public static double percentError(double a, double b) {
+        double error = Math.abs(a - b) / a * 100;
+        return error;
+    }
 }
